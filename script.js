2559888c11ab040582592b39df809dc9d70b8448
yugsharma4 <65899617+yugsharma4@users.noreply.github.com>
2023-02-27 12:39:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your code here
+const h3 = document.querySelector("#wordCount");
+const input = document.querySelector("#evaluatedText");
+let count = 0;
+h3.innerHTML = 0;
+input.addEventListener("input", (e) => {
+  const currentString = e.target.value;
+  const enteredChar = currentString.charAt(currentString.length - 1);
+  if (enteredChar == " ") {
+    count++;
+    h3.innerHTML = count;
+  }
+});
+
